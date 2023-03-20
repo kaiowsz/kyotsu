a4c946eddac6089f72affe11a4965f5f953d3d55
Kaio Silveira <kaionpsa@gmail.com>
2023-03-20 19:32:26 -0300

    styled components config

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav } from './Elements'
+
+function Navbar() {
+  return (
+    <div>
+        <Nav>
+            Hello!
+        </Nav>
+    </div>
+  )
+}
+
+export default Navbar
