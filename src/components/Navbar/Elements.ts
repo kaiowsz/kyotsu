e65faf85c2b3c133d2b4cc586e1bc5721b74fd9d
Kaio Silveira <kaionpsa@gmail.com>
2023-03-21 19:30:59 -0300

    changing colors

--- src/components/Navbar/Elements.ts
+++ src/components/Navbar/Elements.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    background: red;
+    background: black;
+    color: white;
     height: 80px;
     margin-top: 80px;
     display: flex;
