789d86fd3e5a80142cfbd1a62187c288c0126ad4
Kaio Silveira <kaionpsa@gmail.com>
2023-03-24 19:37:14 -0300

    navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './Elements'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavItem, NavLinks, NavMenu, NavBtn, NavBtnLink } from './Elements'
+import { FaBars } from "react-icons/fa"
 
 function Navbar() {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">Something</NavLogo>
+          <NavLogo to="/">kyotsu</NavLogo>
+        <MobileIcon>
+          <FaBars/>
+        </MobileIcon>
+        <NavMenu>
+
+          <NavItem>
+            <NavLinks to="about">About</NavLinks>
+          </NavItem>
+
+          <NavItem>
+            <NavLinks to="discover">Discover</NavLinks>
+          </NavItem>
+
+          <NavItem>
+            <NavLinks to="services">Services</NavLinks>
+          </NavItem>
+
+          <NavItem>
+            <NavLinks to="signup">Sign Up</NavLinks>
+          </NavItem>
+
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
